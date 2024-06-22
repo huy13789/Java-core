1a8fc02a8e1d1171801d10f3279c45fc68ceb362
huy13789 <70091982+huy13789@users.noreply.github.com>
2024-06-22 23:44:11 +0700

    add answer

--- JavaCore/src/main/java/File/FileDemo.java
+++ JavaCore/src/main/java/File/FileDemo.java
@@ -4,8 +4,26 @@ import java.io.IOException;
 
 public class FileDemo {
     public static void main(String[] args) throws IOException {
-        String path = "C:/Users/huy13/OneDrive/Desktop/VTI/JavaCore/JavaCore/src/main/resources/abc.txt";
-        FileManagers.createNewFile(path);
+        //String path = "C:/Users/huy13/OneDrive/Desktop/VTI/JavaCore/JavaCore/src/main/resources";
+        String path = "src/main/resources";
+        System.out.println(FileManagers.exists(path));
+        System.out.println(FileManagers.isFile(path));
+        System.out.println(FileManagers.isDirectory(path));
+        FileManagers.createNewFile(path + "/abc.txt");
+        FileManagers.mkdirs(path + "/database");
+        String[] names = FileManagers.list(path);
+        for (String name : names) {
+            System.out.println("👉 " + name);
+        }
+
+        FileManagers.moveTo(
+                path + "/abc.txt",
+                path + "/database/lesson.sql"
+        );
+
+        FileManagers.delete(path + "/database/lesson.sql");
+        FileManagers.delete(path + "/database");
+        FileManagers.delete(path + "/abc.txt");
     }
 }
 
--- JavaCore/src/main/java/File/FileManagers.java
+++ JavaCore/src/main/java/File/FileManagers.java
@@ -6,12 +6,42 @@ import java.io.IOException;
 public class FileManagers {
     public static boolean createNewFile(String path) throws IOException {
         File file = new File(path);
-        File parentDir = file.getParentFile();
-        // Create the directory structure if it does not exist
-        if (parentDir != null && !parentDir.exists()) {
-            parentDir.mkdirs();
-        }
-
         return file.createNewFile();
+    }
+
+    public static boolean exists(String path) {
+        File file = new File(path);
+        return file.exists();
+    }
+
+    public static boolean isFile(String path) {
+        File file = new File(path);
+        return file.isFile();
+    }
+
+    public static boolean isDirectory(String path) {
+        File file = new File(path);
+        return file.isDirectory();
+    }
+
+    public static String[] list(String path) {
+        File file = new File(path);
+        return file.list();
+    }
+
+    public static boolean delete(String path) {
+        File file = new File(path);
+        return file.delete();
+    }
+
+    public static boolean mkdirs(String path) {
+        File file = new File(path);
+        return file.mkdirs();
+    }
+
+    public static boolean moveTo(String source, String destination) {
+        File from = new File(source);
+        File to = new File(destination);
+        return from.renameTo(to);
     }
 }
